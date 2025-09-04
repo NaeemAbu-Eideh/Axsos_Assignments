6def5c8fc3095365449c537ce78eb44380edd6e1
NaeemAbu-Eideh <naeem.abueideh@gmail.com>
2025-09-04 14:59:19 +0300

    add new assignment named Vedio Preview

---
+++ Web_Funds/Javascript/Vedio_Preview/script.js
@@ -0,0 +1,9 @@
+function onMouseOver(){
+    var vedio = document.querySelector("#vedio");
+    vedio.play();
+}
+
+function onMouseOut(){
+        var vedio = document.querySelector("#vedio");
+        vedio.pause();
+}
