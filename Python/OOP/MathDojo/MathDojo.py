70d04b05332e8c5de39a6c199b2e7d43ac3ec34f
NaeemAbu-Eideh <naeem.abueideh@gmail.com>
2025-09-24 14:47:45 +0300

    add new assignment named MathDojo

---
+++ Python/OOP/MathDojo/MathDojo.py
@@ -0,0 +1,25 @@
+class MathDojo:
+    def __init__(self, result = 0):
+        self.result = result
+    
+    def add(self, number, *numbers):
+        self.result += number
+        i = 0
+        while(i < len(numbers)):
+            self.result += numbers[i]
+            i+=1
+        return self
+    
+    def subtract(self, number, *numbers):
+        self.result -= number
+        i = 0
+        while(i < len(numbers)):
+            self.result-= numbers[i]
+            i+=1
+        return self
+
+x = MathDojo(10)
+print(x.add(1,3,3).add(1,3).add(3,4,5,6).result)
+print(x.subtract(1,3,3).subtract(1).subtract(3,5,6).result)
+
+
