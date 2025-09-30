cfc3d448a02969463565a18cd31d972e69f7545b
NaeemAbu-Eideh <naeem.abueideh@gmail.com>
2025-09-30 23:52:16 +0300

    add new assignment named Dojo Servey in flask

--- Python/Flask_Fundemental/Dojo_Survey/server.py
+++ Python/Flask_Fundemental/Dojo_Survey/server.py
@@ -1,8 +1,18 @@
-from flask import Flask, template_rendered
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
+@app.route("/")
+def input_info():
+    return render_template("sign_information.html")
 
+@app.route('/result', methods=['POST'])
+def create_user():
+    name = request.form["name"]
+    language = request.form["language"]
+    location = request.form["location"]
+    comment = request.form["comment"]    
+    return render_template("show_information.html", name = name, language = language, location = location, comment = comment)
 
 if(__name__ == "__main__"):
     app.run()
