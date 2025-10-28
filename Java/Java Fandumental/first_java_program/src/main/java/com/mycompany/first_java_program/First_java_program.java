537aa142f46af4ed3028d2fd1522a68a2cc92c46
NaeemAbu-Eideh <naeem.abueideh@gmail.com>
2025-10-28 14:48:41 +0200

    new assignment

---
+++ Java/Java Fandumental/first_java_program/src/main/java/com/mycompany/first_java_program/First_java_program.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.first_java_program;
+
+/**
+ *
+ * @author Naeem Abu-Eideh
+ */
+public class First_java_program {
+
+    public static void main(String[] args) {
+        System.out.println("My name is Naeem");
+        System.out.println("I am 24 years old");
+        System.out.println("My hometown is Nablus, Palestine");
+    }
+}
+
+
