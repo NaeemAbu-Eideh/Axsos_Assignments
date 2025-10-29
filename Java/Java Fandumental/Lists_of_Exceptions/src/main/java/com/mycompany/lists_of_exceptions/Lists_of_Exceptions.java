f2306dcdb764a0b94b66726e7368c76e89a37b46
NaeemAbu-Eideh <naeem.abueideh@gmail.com>
2025-10-29 14:26:58 +0200

    new assignment

---
+++ Java/Java Fandumental/Lists_of_Exceptions/src/main/java/com/mycompany/lists_of_exceptions/Lists_of_Exceptions.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package com.mycompany.lists_of_exceptions;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Naeem Abu-Eideh
+ */
+public class Lists_of_Exceptions {
+
+    public static void main(String[] args) {
+        ArrayList<Object> myList = new ArrayList<Object>();
+        myList.add("13");
+        myList.add("hello world");
+        myList.add(48);
+        myList.add("Goodbye World");
+        for (int i = 0; i < myList.size(); i++) {
+            try {
+                Integer castedValue = (Integer) myList.get(i);
+                System.out.println(castedValue);
+            } catch (Exception e) {
+                System.err.println("this an error");
+            }
+        }
+    }
+
+}
