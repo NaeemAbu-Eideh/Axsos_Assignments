02c6dde9740bd3a4a631d000c5172d30fa404d34
NaeemAbu-Eideh <naeem.abueideh@gmail.com>
2025-11-04 19:49:45 +0200

    new assignment

--- Java/Java Fandumental/Caresoft/src/com/caresoft/clinicapp/AdminUser.java
+++ Java/Java Fandumental/Caresoft/src/com/caresoft/clinicapp/AdminUser.java
@@ -3,13 +3,13 @@ package com.caresoft.clinicapp;
 import java.util.ArrayList;
 import java.util.Date;
 
-public class AdminUser extends User implements HIPAACompliantUser, HIPAACompliantAdmin {
+public class AdminUser extends User implements HIPAACompliantUser, HIPAACompliantAdmin {// this class implements the HIPAACompliantUser and HIPAACompliantAdmin interfaces and inherets from User class
 	private Integer employeeID;
     private String role;
     private ArrayList<String> securityIncidents;
     
     // TO DO: Implement a constructor that takes an ID and a role
-    public AdminUser(Integer employeeID, String role) {
+    public AdminUser(Integer employeeID, String role) { // this constructor receive id and role from user and insert it to the mebmer variabels of the class
         this.setId(employeeID);
         this.employeeID = employeeID;
         this.role = role;
@@ -25,7 +25,7 @@ public class AdminUser extends User implements HIPAACompliantUser, HIPAAComplian
         );
         securityIncidents.add(report);
     }
-    public void authIncident() {
+    public void authIncident() { // it came from HIPAACompliantAdmin interface , in this method I created a report and add it to the list of reports
         String report = String.format(
             "Datetime Submitted: %s \n,  ID: %s\n Notes: %s \n", 
             new Date(), this.id, "AUTHORIZATION ATTEMPT FAILED FOR THIS USER"
@@ -39,7 +39,7 @@ public class AdminUser extends User implements HIPAACompliantUser, HIPAAComplian
     }
 
     @Override
-    public boolean assignPin(int pin) {
+    public boolean assignPin(int pin) { // this class come from HIPAACompliantUser interface , in this method I check if the pin length is 6 or not and if true insert it into pin variable
         String strPin = "" + pin;
         if(strPin.length() ==6) {
             this.setPin(pin);
@@ -48,7 +48,7 @@ public class AdminUser extends User implements HIPAACompliantUser, HIPAAComplian
     }
 
     @Override
-    public boolean accessAuthorized(Integer confirmedAuthID) {
+    public boolean accessAuthorized(Integer confirmedAuthID) { // this class come from HIPAACompliantUser interface , in this method I check if the id come from user is match with the id store in the object of this class , and return a boolean result, if the result is false, it created a report and store it to the list of reports
         Integer id = getId();
         if(id.equals(confirmedAuthID)) { return true;}
         else{
@@ -57,10 +57,17 @@ public class AdminUser extends User implements HIPAACompliantUser, HIPAAComplian
         }
     }
 
+    // this is setters and getters for class member variables
     public void setPatientNotes(ArrayList<String> securityIncidents) {
         this.securityIncidents = securityIncidents;
     }
     public ArrayList<String> getPatientNotes() {
         return securityIncidents;
     }
+    public void setRole(String role) {
+        this.role = role;
+    }
+    public String getRole() { return role; }
+    public Integer getEmployeeID() { return employeeID; }
+    public void setEmployeeID(Integer employeeID) { this.employeeID = employeeID; }
 }
--- Java/Java Fandumental/Caresoft/src/com/caresoft/clinicapp/Physician.java
+++ Java/Java Fandumental/Caresoft/src/com/caresoft/clinicapp/Physician.java
@@ -3,13 +3,13 @@ package com.caresoft.clinicapp;
 import java.util.ArrayList;
 import java.util.Date;
 
-public class Physician extends User implements HIPAACompliantUser {
+public class Physician extends User implements HIPAACompliantUser { // this class implements the HIPAACompliantUser interface and inherets from User class
 	private ArrayList<String> patientNotes;
 	
 	// TO DO: Constructor that takes an IDcopy
     public Physician(Integer num){
         this.setId(num);
-    }
+    } //this constructor receive one argument named ID and stor it
     // TO DO: Implement HIPAACompliantUser!
 	
 	public void newPatientNotes(String notes, String patientName, Date date) {
@@ -22,7 +22,7 @@ public class Physician extends User implements HIPAACompliantUser {
     }
 
     @Override
-    public boolean assignPin(int pin) {
+    public boolean assignPin(int pin) { // this class come from HIPAACompliantUser interface , in this method I check if the pin length is 4 or not and if true insert it into pin variable
         String strPin = "" + pin;
         if(strPin.length() == 4){
             this.setPin(pin);
@@ -31,12 +31,13 @@ public class Physician extends User implements HIPAACompliantUser {
     }
 
     @Override
-    public boolean accessAuthorized(Integer confirmedAuthID) {
+    public boolean accessAuthorized(Integer confirmedAuthID) { // this class come from HIPAACompliantUser interface , in this method I check if the id come from user is match with the id store in the object of this class , and return a boolean result
         Integer id = this.getId();
         return id.equals(confirmedAuthID);
     }
 
     // TO DO: Setters & Getters
+    // this is setters and getters for the class member variable
     public void setPatientNotes(ArrayList<String> patientNotes) {
         this.patientNotes = patientNotes;
     }
