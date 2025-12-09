476366529149e3b63c4e5b01b50e347f5ce27922
NaeemAbu-Eideh <naeem.abueideh@gmail.com>
2025-12-09 19:57:37 +0200

    add new assignment

--- MERN/React/coinflipping/src/App.jsx
+++ MERN/React/coinflipping/src/App.jsx
@@ -1,34 +1,44 @@
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
 import './App.css'
 
-function App() {
-    function tossCoin() {
-        return Math.random() > 0.5 ? "heads" : "tails";
-    }
+function tossCoin() {
+    return Math.random() <= 0.5 ? "tails" : "heads";
+}
+function fiveHeads() {
+    return new Promise((resolve, reject) => {
+        let trying = 0;
+        let headCounter = 0;
 
-    function fiveHeads() {
-        return new Promise( (resolve, reject) => {
-            let headsCount = 0;
-            let attempts = 0;
-            while(headsCount < 5) {
-                attempts++;
-                let result = tossCoin();
-                console.log(`${result} was flipped`);
-                if(result === "heads") {
-                    headsCount++;
-                } else {
-                    headsCount = 0;
-                }
+        while (headCounter !== 5) {
+            const head = tossCoin();
+            if (head === "heads") {
+                headCounter++;
+            } else {
+                headCounter = 0;
             }
-            return `It took ${attempts} tries to flip five "heads"`;
-        });
-    }
+            trying += 1;
+        }
 
+        if (trying <= 100) {
+            resolve("successfully got 5 heads in a row after " + trying + " tries");
+        } else {
+            reject("failed, took more than 100 tries (" + trying + ")");
+        }
+    });
+}
 
-    fiveHeads()
-        .then( res => console.log(res) )
-        .catch( err => console.log(err) );
-    console.log( "When does this run now?" );
+function App() {
+    const hasRun = useRef(false);
+    useEffect(() => {
+        if (hasRun.current) return;
+        hasRun.current = true;
+        fiveHeads()
+            .then( res => console.log(res) )
+            .catch( err => console.log(err) );
+        console.log( "When does this run now?" );
+
+    }, []);
+
   return (
     <>
 
