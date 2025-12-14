d0f88d157a8ec3c780866324defc7437d3a2f290
NaeemAbu-Eideh <naeem.abueideh@gmail.com>
2025-12-14 19:31:23 +0200

    add new assignment

--- MERN/Express/Faker_API/server.js
+++ MERN/Express/Faker_API/server.js
@@ -1,19 +1,17 @@
-const { faker } = require('@faker-js/faker');
-
+const { faker } = require("@faker-js/faker");
 const express = require("express");
 const app = express();
 const port = 8000;
 
-app.use( express.json() );
-app.use( express.urlencoded({ extended: true }) );
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 function createRandomUser() {
   return {
-    
-    _Id: faker.string.uuid(),
+    _id: faker.string.uuid(),
     firstName: faker.person.firstName(),
-    lastname: faker.person.lastname(),
-    phone: faker.phone.imei(),
+    lastName: faker.person.lastName(),
+    phone: faker.phone.number(),
     email: faker.internet.email(),
     password: faker.internet.password(),
   };
@@ -21,35 +19,35 @@ function createRandomUser() {
 
 function createRandomCompany() {
   return {
-    
-    _Id: faker.string.uuid(),
+    _id: faker.string.uuid(),
     name: faker.company.name(),
-    address:{
-      street: faker.location.street(),
+    address: {
+      street: faker.location.streetAddress(),
       city: faker.location.city(),
       state: faker.location.state(),
       zipCode: faker.location.zipCode(),
       country: faker.location.country(),
-    }
+    },
   };
 }
 
-users = {};
-companies = {}
+const users = [];
+const companies = [];
 
 app.post("/api/users/new", (req, res) => {
-    users.push(createRandomUser());
-    res.json( { status: "ok" } );
+  const newUser = createRandomUser();
+  users.push(newUser);
+  res.json(newUser);
 });
 
 app.post("/api/companies/new", (req, res) => {
-    companies.push(createRandomCompany());
-    res.json( { status: "ok" } );
+  const newCompany = createRandomCompany();
+  companies.push(newCompany);
+  res.json(newCompany);
 });
 
 app.get("/api/user/company", (req, res) => {
-    res.json( users );
-    res.json( companies );
+  res.json({ users, companies });
 });
 
-app.listen(port, () => console.log(`port is: ${port}`))
+app.listen(port, () => console.log(`port is: ${port}`));
